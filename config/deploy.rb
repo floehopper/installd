d510237493597c2b77b2bdec117c00e843b1e8ca
James Mead <james@floehopper.org>
2009-06-08 20:58:23 +0100

    After first successful deployment using capistrano.

---
+++ config/deploy.rb
@@ -0,0 +1,35 @@
+default_run_options[:pty] = true
+
+set :application, "installd"
+
+set :deploy_to, "/var/rails/#{application}"
+
+set :scm, :git
+set :repository,  "git@git.floehopper.org:#{application}"
+
+server "argonaut.slice", :app, :web, :db, :primary => true
+
+after "deploy:update_code", "symlink:db"
+
+namespace :deploy do
+
+  desc "Restart Application"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+  
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+  
+end
+
+namespace :symlink do
+  
+  desc "symlink database yaml" 
+  task :db do
+    run "ln -s #{shared_path}/config/database.yml #{release_path}/config/database.yml" 
+  end
+  
+end
