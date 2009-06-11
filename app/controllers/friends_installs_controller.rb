c93aae0fc40c1873f208c4f29bf3401ff0fba07e
James Mead <james@floehopper.org>
2009-06-11 18:05:00 +0100

    Paginate lists of installed apps.

--- app/controllers/friends_installs_controller.rb
+++ app/controllers/friends_installs_controller.rb
@@ -2,7 +2,7 @@ class FriendsInstallsController < ApplicationController
   
   def index
     @user = User.find_by_login(params[:user_id])
-    @friends_installs = @user.friends_installs.all(:include => :app)
+    @friends_installs = @user.friends_installs.paginate(:include => :app, :page => params[:page], :per_page => 10)
   end
   
 end
--- app/controllers/installs_controller.rb
+++ app/controllers/installs_controller.rb
@@ -2,7 +2,7 @@ class InstallsController < ApplicationController
   
   def index
     @user = User.find_by_login(params[:user_id])
-    @installs = @user.installs.all(:include => :app)
+    @installs = @user.installs.paginate(:include => :app, :page => params[:page], :per_page => 10)
   end
     
 end
