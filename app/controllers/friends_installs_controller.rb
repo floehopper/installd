a2e00f01f2a50f00d340c68f0e88d9e7e8089813
James Mead <james@floehopper.org>
2009-06-26 12:58:04 +0100

    Group friends' installs by app and display count of installs per app.

--- app/controllers/friends_installs_controller.rb
+++ app/controllers/friends_installs_controller.rb
@@ -2,7 +2,8 @@ class FriendsInstallsController < ApplicationController
   
   def index
     @user = User.find_by_login(params[:user_id])
-    @friends_installs = @user.friends_installs.paginate(:include => :app, :page => params[:page], :per_page => 10)
+    apps_vs_friends_installs = @user.friends_installs(:include => :app).group_by(&:app).sort_by { |app, installs| -installs.length }
+    @friends_installs = apps_vs_friends_installs.paginate(:page => params[:page], :per_page => 10)
   end
   
 end
