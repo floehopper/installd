85344a7bfe637afd1295f4038a1d9a63d79cf7e4
James Mead <james@floehopper.org>
2009-06-26 11:38:50 +0100

    Add a synchronize method for client to talk to. Currently client just passes a concatenated set of plist xml for all apps. Currently all installs get removed and re-added. It would be better if existing installs were updated, missing installs were added, and extra installs were removed.

--- app/controllers/installs_controller.rb
+++ app/controllers/installs_controller.rb
@@ -1,3 +1,5 @@
+require 'hpricot'
+
 class InstallsController < ApplicationController
   
   def index
@@ -8,5 +10,19 @@ class InstallsController < ApplicationController
       @installs = Install.paginate(:include => :app, :page => params[:page], :per_page => 10)
     end
   end
-    
+  
+  def synchronize
+    user = User.find_by_login(params[:user_id])
+    user.installs.clear
+    doc = Hpricot::XML(params[:doc])
+    (doc/'plist').each do |element|
+      itemName = (element/'dict'/"key[text()='itemName']")[0].next_sibling.inner_text
+      itemId = (element/'dict'/"key[text()='itemId']")[0].next_sibling.inner_text
+      softwareIcon57x57URL = (element/'dict'/"key[text()='softwareIcon57x57URL']")[0].next_sibling.inner_text
+      app = App.find_by_name(itemName) || App.create!(:name => itemName, :item_id => itemId, :icon_url => softwareIcon57x57URL)
+      user.installs.create!(:app => app)
+    end
+    render :nothing => true, :status => :ok
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ ActionController::Routing::Routes.draw do |map|
   
   map.resources :users, :only => [:show, :new, :create] do |users|
     users.resources :friendships, :only => [:new, :create]
-    users.resources :installs, :only => [:index]
+    users.resources :installs, :only => [:index], :collection => { :synchronize => :put }
     users.resources :friends_installs, :only => [:index]
   end
   
