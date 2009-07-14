ee10c0e77ffd0287574fda733e6a4e3b1c92a7b8
James Mead <james@floehopper.org>
2009-07-14 12:05:49 +0100

    Added registration and activation emails. Also tweaked invitation email.

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,12 +1,30 @@
 class UserMailer < ActionMailer::Base
   
+  def registration(user)
+    recipients user.email
+    subject 'installd.com registration'
+    from 'registrations@installd.com'
+    reply_to 'registrations@installd.com'
+    headers({ 'Return-Path' => 'registrations@installd.com' })
+    body({ :root_url => root_url(:host => HOST)})
+  end
+  
   def invitation(user)
     recipients user.email
-    subject 'Installd.com invitation'
+    subject 'installd.com invitation'
     from 'invitations@installd.com'
     reply_to 'invitations@installd.com'
     headers({ 'Return-Path' => 'invitations@installd.com' })
-    body({ :activation_url => new_activation_url(:code => user.perishable_token, :host => HOST) })
+    body({ :root_url => root_url(:host => HOST), :activation_url => new_activation_url(:code => user.perishable_token, :host => HOST) })
+  end
+  
+  def activation(user)
+    recipients user.email
+    subject 'installd.com activation'
+    from 'activations@installd.com'
+    reply_to 'activations@installd.com'
+    headers({ 'Return-Path' => 'activations@installd.com' })
+    body({ :root_url => root_url(:host => HOST), :downloads_url => downloads_url(:host => HOST) })
   end
   
 end
