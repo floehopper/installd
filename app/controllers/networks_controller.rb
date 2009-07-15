64cd1373f3ba4b2c31543d354cd7452aa8a91cd9
James Mead <james@floehopper.org>
2009-07-15 10:27:45 +0100

    Reduce duplication in NetworksController.

--- app/controllers/networks_controller.rb
+++ app/controllers/networks_controller.rb
@@ -1,25 +1,35 @@
 class NetworksController < ApplicationController
   
+  before_filter :load_user
+  
   def show
-    @user = User.find_by_login(params[:user_id], :include => { :installs => :app })
-    apps_vs_friends_installs = @user.friends_installs(:include => :app).group_by(&:app).sort_by { |app, installs| -installs.length }
-    @friends_installs = apps_vs_friends_installs.paginate(:page => params[:page], :per_page => 10)
+    load_friends_installs
   end
   
   def in_common
-    @user = User.find_by_login(params[:user_id], :include => { :installs => :app })
-    apps_vs_friends_installs = @user.friends_installs(:include => :app).group_by(&:app).sort_by { |app, installs| -installs.length }
-    apps_vs_friends_installs.reject! { |app, installs| !@user.apps.include?(app) }
-    @friends_installs = apps_vs_friends_installs.paginate(:page => params[:page], :per_page => 10)
+    load_friends_installs do |apps_vs_friends_installs|
+      apps_vs_friends_installs.reject! { |app, installs| !@user.apps.include?(app) }
+    end
     render :action => 'show'
   end
   
   def not_in_common
-    @user = User.find_by_login(params[:user_id], :include => { :installs => :app })
-    apps_vs_friends_installs = @user.friends_installs(:include => :app).group_by(&:app).sort_by { |app, installs| -installs.length }
-    apps_vs_friends_installs.reject! { |app, installs| @user.apps.include?(app) }
-    @friends_installs = apps_vs_friends_installs.paginate(:page => params[:page], :per_page => 10)
+    load_friends_installs do |apps_vs_friends_installs|
+      apps_vs_friends_installs.reject! { |app, installs| @user.apps.include?(app) }
+    end
     render :action => 'show'
+  end
+  
+  private
+  
+  def load_user
+    @user = User.find_by_login(params[:user_id], :include => { :installs => :app })
+  end
+  
+  def load_friends_installs
+    apps_vs_friends_installs = @user.friends_installs(:include => :app).group_by(&:app).sort_by { |app, installs| -installs.length }
+    yield apps_vs_friends_installs if block_given?
+    @friends_installs = apps_vs_friends_installs.paginate(:page => params[:page], :per_page => 10)
   end
   
 end
