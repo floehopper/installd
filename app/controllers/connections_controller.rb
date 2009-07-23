b68c238b35f07aa04498a52dca0703c1ae938dab
James Mead <james@floehopper.org>
2009-07-23 17:10:07 +0100

    Added the ability to remove a user from your network. And more tweaks to layouts of forms. Flash notice changed to a div instead of a paragraph to avoid margins taking up space at top of some pages.

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -8,11 +8,38 @@ class ConnectionsController < ApplicationController
   def create
     @connected_user = User.find_by_login(params[:user_id])
     @connection = Connection.new(:user => current_user, :connected_user => @connected_user)
-    if @connection.save
-      flash[:notice] = "#{@connected_user.login} has been added to your network."
-      redirect_to @connected_user
+    if params[:commit] == 'Yes'
+      if @connection.save
+        flash[:notice] = "#{@connected_user.login} has been added to your network."
+        redirect_to @connected_user
+      else
+        flash[:notice] = "Failed to add #{@connected_user.login} from your network."
+        render :action => 'new'
+      end
     else
-      render :action => 'new'
+      flash[:notice] = nil
+      redirect_to @connected_user
+    end
+  end
+  
+  def delete
+    @connected_user = User.find_by_login(params[:user_id])
+  end
+  
+  def destroy
+    @connected_user = User.find_by_login(params[:user_id])
+    @connection = current_user.connections.find_by_connected_user_id(@connected_user.id)
+    if params[:commit] == 'Yes'
+      if @connected_user && @connection && @connection.destroy
+        flash[:notice] = "#{@connected_user.login} has been removed from your network."
+        redirect_to @connected_user
+      else
+        flash[:notice] = "Failed to remove #{@connected_user.login} from your network."
+        render :action => 'new'
+      end
+    else
+      flash[:notice] = nil
+      redirect_to @connected_user
     end
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   
   map.resources :users, :only => [:show, :new, :create] do |users|
-    users.resources :connections, :only => [:new, :create]
+    users.resources :connections, :only => [:new, :create, :destroy], :member => { :delete => :get }
     users.resources :installs, :only => [:index], :collection => { :synchronize => :put }
     users.resource :network, :only => [:show], :member => { :in_common => :get, :not_in_common => :get }
   end
