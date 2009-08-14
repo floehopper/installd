656e6aee09cc7623d020ec7d1dc0dc9fcb4d3c9f
James Mead <james@floehopper.org>
2009-08-14 17:42:05 +0100

    Extract helper methods for common chunks of html.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
-# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def icon(app)
+    link_to(image_tag(app.icon.url, :alt => app.name), app.url, :class => 'appIcon')
+  end
+  
+  def heading(app)
+    content_tag('h3', :class => 'appName') do
+      link_to truncate(app.name), app_path(app)
+    end
+  end
+  
 end
