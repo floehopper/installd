6f0add9a6084c4193981677f9bd66558d5bbaef3
James Mead <james@floehopper.org>
2009-09-11 12:57:41 +0100

    Optimize SQL on network pages.

--- app/controllers/networks_controller.rb
+++ app/controllers/networks_controller.rb
@@ -3,27 +3,60 @@ class NetworksController < ApplicationController
   before_filter :load_user
   
   def show
-    my_apps = Set.new(@user.apps)
-    network_apps = Set.new(@user.connected_apps)
-    now = Time.now
-    @apps = (network_apps + my_apps).sort_by { |app| now - app.installs.map(&:created_at).max }.paginate(:page => params[:page], :per_page => 15)
+    all_apps = App.find_by_sql(%{
+      SELECT apps.*, COUNT(installs.id) AS number_of_installs, AVG(installs.rating) AS average_rating, MAX(installs.created_at) AS maximum_created_at FROM apps
+        INNER JOIN installs ON (apps.id = installs.app_id)
+        WHERE apps.id IN (SELECT apps.id FROM apps
+          INNER JOIN installs ON (apps.id = installs.app_id)
+          INNER JOIN users ON (installs.user_id = #{@user.id}))
+        OR apps.id IN (SELECT apps.id FROM apps
+          INNER JOIN installs ON (apps.id = installs.app_id)
+          INNER JOIN users ON (installs.user_id = users.id)
+          INNER JOIN connections ON (users.id = connections.connected_user_id)
+          WHERE (connections.user_id = #{@user.id}))
+        GROUP BY apps.id
+        ORDER BY maximum_created_at DESC
+    })
+    @apps = all_apps.paginate(:page => params[:page], :per_page => 15)
     @rss_feed_url = url_for(:format => 'rss')
   end
   
   def in_common
-    my_apps = Set.new(@user.apps)
-    network_apps = Set.new(@user.connected_apps)
-    now = Time.now
-    @apps = (network_apps & my_apps).sort_by { |app| now - app.installs.map(&:created_at).max }.paginate(:page => params[:page], :per_page => 15)
+    apps_in_common = App.find_by_sql(%{
+      SELECT apps.*, COUNT(installs.id) AS number_of_installs, AVG(installs.rating) AS average_rating, MAX(installs.created_at) AS maximum_created_at FROM apps
+        INNER JOIN installs ON (apps.id = installs.app_id)
+        WHERE apps.id IN (SELECT apps.id FROM apps
+          INNER JOIN installs ON (apps.id = installs.app_id)
+          INNER JOIN users ON (installs.user_id = #{@user.id}))
+        AND apps.id IN (SELECT apps.id FROM apps
+          INNER JOIN installs ON (apps.id = installs.app_id)
+          INNER JOIN users ON (installs.user_id = users.id)
+          INNER JOIN connections ON (users.id = connections.connected_user_id)
+          WHERE (connections.user_id = #{@user.id}))
+        GROUP BY apps.id
+        ORDER BY maximum_created_at DESC
+    })
+    @apps = apps_in_common.paginate(:page => params[:page], :per_page => 15)
     @rss_feed_url = url_for(:format => 'rss')
     render :action => 'show'
   end
   
   def not_in_common
-    my_apps = Set.new(@user.apps)
-    network_apps = Set.new(@user.connected_apps)
-    now = Time.now
-    @apps = (network_apps - my_apps).sort_by { |app| now - app.installs.map(&:created_at).max }.paginate(:page => params[:page], :per_page => 15)
+    apps_not_in_common = App.find_by_sql(%{
+      SELECT apps.*, COUNT(installs.id) AS number_of_installs, AVG(installs.rating) AS average_rating, MAX(installs.created_at) AS maximum_created_at FROM apps
+        INNER JOIN installs ON (apps.id = installs.app_id)
+        WHERE apps.id NOT IN (SELECT apps.id FROM apps
+          INNER JOIN installs ON (apps.id = installs.app_id)
+          INNER JOIN users ON (installs.user_id = #{@user.id}))
+        AND apps.id IN (SELECT apps.id FROM apps
+          INNER JOIN installs ON (apps.id = installs.app_id)
+          INNER JOIN users ON (installs.user_id = users.id)
+          INNER JOIN connections ON (users.id = connections.connected_user_id)
+          WHERE (connections.user_id = #{@user.id}))
+        GROUP BY apps.id
+        ORDER BY maximum_created_at DESC
+    })
+    @apps = apps_not_in_common.paginate(:page => params[:page], :per_page => 15)
     @rss_feed_url = url_for(:format => 'rss')
     render :action => 'show'
   end
@@ -31,7 +64,7 @@ class NetworksController < ApplicationController
   private
   
   def load_user
-    @user = User.find_by_login(params[:user_id], :include => { :installs => :app })
+    @user = User.find_by_login(params[:user_id])
   end
   
 end
