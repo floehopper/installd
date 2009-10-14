29db4fb10f75778d9c9ab0052489f91107ba32f1
James Mead <james@floehopper.org>
2009-10-14 09:24:19 +0100

    Explore the responses from the app store web server.

---
+++ lib/app_store_debugger.rb
@@ -0,0 +1,21 @@
+def puts_with_indent(text, indent)
+  puts "#{(' ' * indent)}#{text}"
+end
+
+def display(element, indent)
+  if element.respond_to?(:each_child)
+    puts_with_indent(element.name, indent)
+    element.each_child { |child| display(child, indent + 1) }
+  else
+    text = element.to_s.chomp.strip
+    puts_with_indent(text, indent) unless text.empty?
+  end
+end
+
+http = SimpleHttp.new(App.first.url)
+http.request_headers['User-Agent'] = 'iTunes/8.2.1 (Macintosh; U; PPC Mac OS X 10.5.8)'
+response = http.get
+doc = Hpricot::XML(response)
+root = ((doc/'Document')[0]/'View')[0]
+display(root, 0)
+
