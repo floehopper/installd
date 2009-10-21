9d04714ace19ea6147a7f8de75ab45e736c55096
James Mead <james@floehopper.org>
2009-10-21 16:57:31 +0100

    Backup db every night at 4.30am

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,5 @@
 set :output, File.expand_path('schedule.log', File.join(Rails.root, 'log'))
 
-# every 1.day, :at => '4:30 am' do
-every 5.minutes do
+every 1.day, :at => '4:30 am' do
   rake "db:backup"
 end
