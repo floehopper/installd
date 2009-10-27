f934765a987a6d54e708391b4a618d667687989a
James Mead <james@floehopper.org>
2009-10-27 15:27:29 +0000

    Move route for installs update onto a separate non-nested resource for clarity which means there's no need for the shallow flag.

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,16 @@ ActionController::Routing::Routes.draw do |map|
   
   map.resources :users, :only => [:index, :show, :new, :create], :member => { :invite => :put } do |users|
     users.resources :connections, :only => [:new, :create, :destroy], :member => { :delete => :get }
-    users.resources :installs, :only => [:index, :update], :collection => { :synchronize => :put }, :shallow => true
+    users.resources :installs, :only => [:index], :collection => { :synchronize => :put }
     users.resource :network, :only => [:show], :member => { :in_common => :get, :not_in_common => :get }
   end
   
   map.resources :apps, :only => [:show], :collection => { :recent => :get, :popular => :get, :rated => :get }
-  map.resources :installs, :only => [:index]
-  
+  map.resources :installs, :only => [:index, :update]
   map.resources :activations, :only => [:new, :create]
+  map.resource :user_session, :only => [:new, :create, :destroy], :member => { :delete => :get }
   
   map.resources :downloads, :only => [:index]
-  
-  map.resource :user_session, :only => [:new, :create, :destroy], :member => { :delete => :get }
   
   map.lookup_user '/lookup_user', :controller => 'users', :action => 'lookup'
   
