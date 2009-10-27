a7b1a493956d5a2b9e88879483bb92296acbf020
James Mead <james@floehopper.org>
2009-10-27 15:35:48 +0000

    Reformatted routes file to try and have one route per line.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,69 @@
 ActionController::Routing::Routes.draw do |map|
   
-  map.resources :users, :only => [:index, :show, :new, :create], :member => { :invite => :put } do |users|
-    users.resources :connections, :only => [:new, :create, :destroy], :member => { :delete => :get }
-    users.resources :installs, :only => [:index], :collection => { :synchronize => :put }
-    users.resource :network, :only => [:show], :member => { :in_common => :get, :not_in_common => :get }
+  map.resources :users,
+    :only => [
+      :index,
+      :show,
+      :new,
+      :create
+    ],
+    :member => {
+      :invite => :put
+    } do |users|
+      users.resources :connections,
+        :only => [
+          :new,
+          :create,
+          :destroy
+        ],
+        :member => {
+          :delete => :get
+        }
+      users.resources :installs,
+        :only => [
+          :index
+        ],
+        :collection => {
+          :synchronize => :put
+        }
+      users.resource :network,
+        :only => [
+          :show
+        ],
+        :member => {
+          :in_common => :get,
+          :not_in_common => :get
+        }
   end
   
-  map.resources :apps, :only => [:show], :collection => { :recent => :get, :popular => :get, :rated => :get }
-  map.resources :installs, :only => [:index, :update]
-  map.resources :activations, :only => [:new, :create]
-  map.resource :user_session, :only => [:new, :create, :destroy], :member => { :delete => :get }
+  map.resources :apps,
+    :only => [
+      :show
+    ],
+    :collection => {
+      :recent => :get,
+      :popular => :get,
+      :rated => :get
+    }
+  map.resources :installs,
+    :only => [
+      :index,
+      :update
+    ]
+  map.resources :activations,
+    :only => [
+      :new,
+      :create
+    ]
+  map.resource :user_session,
+    :only => [
+      :new,
+      :create,
+      :destroy
+    ],
+    :member => {
+      :delete => :get
+    }
   
   map.lookup_user '/lookup_user', :controller => 'users', :action => 'lookup'
   
