55bf559edb8a26ee255a86b0b71587d1376b5cc5
James Mead <james@floehopper.org>
2009-10-30 19:01:49 +0000

    By using User#synchronize for setting up the tests, we're more likely to create the right models in the right states. By extracting assert_only_last_event_is_current we better explain the intention of the assertion.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,10 +12,12 @@ describe User, 'sync apps' do
     event_attributes = Factory.attributes_for(:event_from_xml)
     
     sync = Factory(:sync, :user => @user)
-    @user.synchronize([app_attributes.merge(event_attributes)], sync)
+    
+    assert_difference('App.count', 1) do
+      @user.synchronize([app_attributes.merge(event_attributes)], sync)
+    end
     
     app = App.first
-    app.should_not be_nil
     App.extract_attributes(app.attributes).should == app_attributes
   end
   
@@ -23,13 +25,15 @@ describe User, 'sync apps' do
     app_attributes = Factory.attributes_for(:app, :name => 'does-exist')
     app = App.create!(app_attributes)
     event_attributes = Factory.attributes_for(:event_from_xml)
+    
     sync_0 = Factory(:sync, :user => @user)
-    @user.events.create!(event_attributes.merge(:current => true, :state => 'Initial', :app => app, :sync => sync_0))
+    @user.synchronize([app_attributes.merge(event_attributes)], sync_0)
     
     sync_1 = Factory(:sync, :user => @user)
-    @user.synchronize([app_attributes.merge(event_attributes)], sync_1)
     
-    App.all.should == [app]
+    assert_difference('App.count', 0) do
+      @user.synchronize([app_attributes.merge(event_attributes)], sync_1)
+    end
   end
   
 end
@@ -46,12 +50,12 @@ describe User, 'sync events' do
     event_attributes = Factory.attributes_for(:event_from_xml)
     sync = Factory(:sync, :user => @user)
     
-    @user.synchronize([app_attributes.merge(event_attributes)], sync)
+    assert_difference('@user.events(reload = true).size', 1) do
+      @user.synchronize([app_attributes.merge(event_attributes)], sync)
+    end
     
-    @user.events.size.should == 1
-    @user.events(reload = true).map(&:current).should == [true]
+    assert_only_last_event_is_current(@user.events)
     event = @user.events.first
-    event.should_not be_nil
     event.state.should == 'Initial'
     Event.extract_attributes(event.attributes).should == event_attributes
   end
@@ -60,31 +64,35 @@ describe User, 'sync events' do
     app_attributes = Factory.attributes_for(:app)
     app = App.create!(app_attributes)
     event_attributes = Factory.attributes_for(:event_from_xml)
+    
     sync_0 = Factory(:sync, :user => @user)
-    event = @user.events.create!(event_attributes.merge(:current => true, :state => 'Initial', :app => app, :sync => sync_0))
+    @user.synchronize([app_attributes.merge(event_attributes)], sync_0)
     
     sync_1 = Factory(:sync, :user => @user)
-    @user.synchronize([app_attributes.merge(event_attributes)], sync_1)
     
-    @user.events.should == [event]
-    @user.events(reload = true).map(&:current).should == [true]
+    assert_difference('@user.events(reload = true).size', 0) do
+      @user.synchronize([app_attributes.merge(event_attributes)], sync_1)
+    end
   end
   
   it 'should create event if user installed app, synced, updated app, and synced' do
     app_attributes = Factory.attributes_for(:app)
     app = App.create!(app_attributes)
     event_attributes = Factory.attributes_for(:event_from_xml, :raw_xml => '<xml>original</xml>')
+    
     Time.stub!(:now).and_return(Time.parse('2009-01-01'))
     sync_0 = Factory(:sync, :user => @user)
-    event = @user.events.create!(event_attributes.merge(:current => true, :state => 'Initial', :app => app, :sync => sync_0))
+    @user.synchronize([app_attributes.merge(event_attributes)], sync_0)
+    
     new_event_attributes = Factory.attributes_for(:event_from_xml, :raw_xml => '<xml>changed</xml>')
     Time.stub!(:now).and_return(Time.parse('2009-01-02'))
-    
     sync_1 = Factory(:sync, :user => @user)
-    @user.synchronize([app_attributes.merge(new_event_attributes)], sync_1)
     
-    @user.events.length.should == 2
-    @user.events(reload = true).map(&:current).should == [false, true]
+    assert_difference('@user.events(reload = true).size', 1) do
+      @user.synchronize([app_attributes.merge(new_event_attributes)], sync_1)
+    end
+    
+    assert_only_last_event_is_current(@user.events)
     new_event = @user.events.last
     new_event.state.should == 'Update'
     Event.extract_attributes(new_event.attributes).should == new_event_attributes
@@ -94,21 +102,25 @@ describe User, 'sync events' do
     app_attributes = Factory.attributes_for(:app)
     app = App.create!(app_attributes)
     event_attributes = Factory.attributes_for(:event_from_xml, :raw_xml => '<xml>original</xml>')
+    
     Time.stub!(:now).and_return(Time.parse('2009-01-01'))
     sync_0 = Factory(:sync, :user => @user)
-    event = @user.events.create!(event_attributes.merge(:current => false, :state => 'Initial', :app => app, :sync => sync_0))
+    @user.synchronize([app_attributes.merge(event_attributes)], sync_0)
+    
     new_event_attributes = Factory.attributes_for(:event_from_xml, :raw_xml => '<xml>changed</xml>')
     Time.stub!(:now).and_return(Time.parse('2009-01-02'))
     sync_1 = Factory(:sync, :user => @user)
-    new_event = @user.events.create!(new_event_attributes.merge(:current => true, :state => 'Updated', :app => app, :sync => sync_1))
+    @user.synchronize([app_attributes.merge(new_event_attributes)], sync_1)
+    
     another_event_attributes = Factory.attributes_for(:event_from_xml, :raw_xml => '<xml>original</xml>')
     Time.stub!(:now).and_return(Time.parse('2009-01-03'))
-    
     sync_2 = Factory(:sync, :user => @user)
-    @user.synchronize([app_attributes.merge(another_event_attributes)], sync_2)
     
-    @user.events.length.should == 3
-    @user.events(reload = true).map(&:current).should == [false, false, true]
+    assert_difference('@user.events(reload = true).size', 1) do
+      @user.synchronize([app_attributes.merge(another_event_attributes)], sync_2)
+    end
+    
+    assert_only_last_event_is_current(@user.events)
     another_event = @user.events.last
     another_event.state.should == 'Update'
     Event.extract_attributes(another_event.attributes).should == another_event_attributes
@@ -118,16 +130,19 @@ describe User, 'sync events' do
     app_attributes = Factory.attributes_for(:app)
     app = App.create!(app_attributes)
     event_attributes = Factory.attributes_for(:event_from_xml)
+    
     Time.stub!(:now).and_return(Time.parse('2009-01-01'))
     sync_0 = Factory(:sync, :user => @user)
-    event = @user.events.create!(event_attributes.merge(:current => true, :state => 'Initial', :app => app, :sync => sync_0))
+    @user.synchronize([app_attributes.merge(event_attributes)], sync_0)
+    
     Time.stub!(:now).and_return(Time.parse('2009-01-02'))
-    
     sync_1 = Factory(:sync, :user => @user)
-    @user.synchronize([], sync_1)
     
-    @user.events.length.should == 2
-    @user.events(reload = true).map(&:current).should == [false, true]
+    assert_difference('@user.events(reload = true).size', 1) do
+      @user.synchronize([], sync_1)
+    end
+    
+    assert_only_last_event_is_current(@user.events)
     uninstall_event = @user.events.last
     uninstall_event.state.should == 'Uninstall'
     Event.extract_attributes(uninstall_event.attributes).should == event_attributes
@@ -137,19 +152,23 @@ describe User, 'sync events' do
     app_attributes = Factory.attributes_for(:app)
     app = App.create!(app_attributes)
     event_attributes = Factory.attributes_for(:event_from_xml)
+    
     Time.stub!(:now).and_return(Time.parse('2009-01-01'))
     sync_0 = Factory(:sync, :user => @user)
-    event = @user.events.create!(event_attributes.merge(:current => false, :state => 'Initial', :app => app, :sync => sync_0))
+    @user.synchronize([app_attributes.merge(event_attributes)], sync_0)
+    
     Time.stub!(:now).and_return(Time.parse('2009-01-02'))
     sync_1 = Factory(:sync, :user => @user)
-    uninstall_event = @user.events.create!(event_attributes.merge(:current => true, :state => 'Uninstall', :state => 'Uninstall', :app => app, :sync => sync_1))
+    @user.synchronize([], sync_1)
+    
     Time.stub!(:now).and_return(Time.parse('2009-01-03'))
-    
     sync_2 = Factory(:sync, :user => @user)
-    @user.synchronize([app_attributes.merge(event_attributes)], sync_2)
     
-    @user.events.length.should == 3
-    @user.events(reload = true).map(&:current).should == [false, false, true]
+    assert_difference('@user.events(reload = true).size', 1) do
+      @user.synchronize([app_attributes.merge(event_attributes)], sync_2)
+    end
+    
+    assert_only_last_event_is_current(@user.events)
     reinstall_event = @user.events.last
     reinstall_event.state.should == 'Install'
     Event.extract_attributes(reinstall_event.attributes).should == event_attributes
@@ -159,22 +178,33 @@ describe User, 'sync events' do
     app_attributes = Factory.attributes_for(:app)
     app = App.create!(app_attributes)
     event_attributes = Factory.attributes_for(:event_from_xml)
+    
     Time.stub!(:now).and_return(Time.parse('2009-01-01'))
     sync_0 = Factory(:sync, :user => @user)
-    event = @user.events.create!(event_attributes.merge(:current => false, :state => 'Initial', :app => app, :sync => sync_0))
+    @user.synchronize([app_attributes.merge(event_attributes)], sync_0)
+    
     Time.stub!(:now).and_return(Time.parse('2009-01-02'))
     sync_1 = Factory(:sync, :user => @user)
-    uninstall_event = @user.events.create!(event_attributes.merge(:current => true, :state => 'Uninstall', :state => 'Uninstall', :app => app, :sync => sync_1))
+    @user.synchronize([], sync_1)
+    
     Time.stub!(:now).and_return(Time.parse('2009-01-03'))
-    
     sync_2 = Factory(:sync, :user => @user)
-    @user.synchronize([], sync_2)
     
-    @user.events.length.should == 2
-    @user.events(reload = true).map(&:current).should == [false, true]
+    assert_difference('@user.events(reload = true).size', 0) do
+      @user.synchronize([], sync_2)
+    end
+    
+    assert_only_last_event_is_current(@user.events)
     uninstall_event = @user.events.last
     uninstall_event.state.should == 'Uninstall'
     Event.extract_attributes(uninstall_event.attributes).should == event_attributes
+  end
+  
+  def assert_only_last_event_is_current(events)
+    other_events = events.dup
+    last_event = other_events.pop
+    other_events.any? { |e| e.current? }.should be_false
+    last_event.should be_current
   end
   
 end
