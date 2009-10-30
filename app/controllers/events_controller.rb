292e34518b6f79b2beb1b345d2d86a84dab10d54
James Mead <james@floehopper.org>
2009-10-30 18:02:25 +0000

    Re-enable syncs.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,23 +13,23 @@ class EventsController < ApplicationController
   end
   
   def synchronize
-  #   user = User.find_by_login(params[:user_id])
-  #   sync = nil
-  #   if current_user && (current_user == user)
-  #     sync = user.syncs.create!
-  #     parser = MultipleIphoneAppPlistParser.new(params[:doc])
-  #     apps = parser.unique_apps
-  #     user.synchronize(apps, sync)
-  #     status = :ok
-  #   else
-  #     status = :forbidden
-  #   end
-  # rescue => e
-  #   logger.error(e)
-  #   logger.info(e.backtrace.join("\n"))
+    user = User.find_by_login(params[:user_id])
+    sync = nil
+    if current_user && (current_user == user)
+      sync = user.syncs.create!
+      parser = MultipleIphoneAppPlistParser.new(params[:doc])
+      apps = parser.unique_apps
+      user.synchronize(apps, sync)
+      status = :ok
+    else
+      status = :forbidden
+    end
+  rescue => e
+    logger.error(e)
+    logger.info(e.backtrace.join("\n"))
     status = :internal_server_error
-  # ensure
-  #   sync.update_attributes(:status => status.to_s) if sync
+  ensure
+    sync.update_attributes(:status => status.to_s) if sync
     render :nothing => true, :status => status
   end
   
