f5db8ed2726b32d5c1a0639164fb9ef7ec5de566
James Mead <james@floehopper.org>
2009-11-04 10:21:01 +0000

    Use app identifier rather than to_param.

--- features/step_definitions/install_steps.rb
+++ features/step_definitions/install_steps.rb
@@ -1,4 +1,4 @@
 Then /^I should see an application with name "([^\"]*)"$/ do |name|
   app = App.find_by_name(name)
-  response.should have_tag("##{app.to_param}")
+  response.should have_tag("##{app.identifier}")
 end
