fc064d941ff96e351d337497643fa2d26ddedb32
James Mead <james@floehopper.org>
2009-11-04 10:49:14 +0000

    Extracted before_filter.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,10 +2,10 @@ class EventsController < ApplicationController
   
   ssl_required :synchronize
   
+  before_filter :load_user
+  
   def index
-    user_id = params[:user_id]
-    if user_id
-      @user = User.find_by_login(user_id)
+    if @user
       @events = @user.events.paginate(:include => [:app, :user], :order => 'created_at DESC', :page => params[:page], :per_page => 15)
     else
       @events = Event.paginate(:all, :include => [:app, :user], :order => 'created_at DESC', :page => params[:page], :per_page => 15)
@@ -14,10 +14,9 @@ class EventsController < ApplicationController
   
   def synchronize
     sync_session = nil
-    user = User.find_by_login(params[:user_id])
-    if user
-      sync_session = user.sync_sessions.create!(:raw_xml => params[:doc])
-      if user == current_user
+    if @user
+      sync_session = @user.sync_sessions.create!(:raw_xml => params[:doc])
+      if @user == current_user
         sync_session.send_later(:parse)
         render :nothing => true, :status => :ok
       else
@@ -30,6 +29,12 @@ class EventsController < ApplicationController
   rescue => e
     sync_session.update_attributes(:status => "exception: #{e}") if sync_session
     raise e
+  end
+  
+  private
+  
+  def load_user
+    @user = User.find_by_login_and_active(params[:user_id], true)
   end
   
 end
